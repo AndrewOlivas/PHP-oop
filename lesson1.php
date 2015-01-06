9288c1e0cbd995ae2f4864ab3a9aa89ab6fbca5c
Andrew Olivas <andrewrolivas@gmail.com>
2015-01-06 09:15:10 -0800

    lesson 1

--- lesson1.php
+++ lesson1.php
@@ -1,5 +1,18 @@
 	class Dog {
-		//class body
-		//class body
-		//class body
+		public $firstName = “default name”;
+		public $lastName = “default last name”;
+		public $gender = “male”;
+		public $price = 0;
+
+	function getName() {
+		return “{$this­>firstName}” .
+		“{$this­>lastName}”;
+		}
 	}
+
+$dog1 = new Dog();
+$dog1­>firstName = “BooBoo”;
+$dog1­>lastName = “Iwao”;
+print “The dog’s name is {$dog1­>getName()}.”;
+
+//The dog’s name is BooBoo Iwao.
