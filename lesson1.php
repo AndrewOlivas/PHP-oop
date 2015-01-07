7f164d32203ec34c266fa71151b58b95d2f91379
Andrew Olivas <andrewrolivas@gmail.com>
2015-01-07 08:39:58 -0800

    lesson 2 up, need more examples

--- lesson1.php
+++ lesson1.php
@@ -11,16 +11,36 @@
 		}
 	}
 
-$dog1 = new Dog();
-$dog1­>firstName = “Barry”;
-$dog1­>lastName = “Allen”;
-print “The dog’s name is {$dog1­>getName()}.”;
+	$dog1 = new Dog();
+	$dog1­>firstName = “Barry”;
+	$dog1­>lastName = “Allen”;
+	print “The dog’s name is {$dog1­>getName()}.”;
 
 //The dog’s name should be BARRY ALLEN
 
-$name = new Name();
-$name­>firstName = “Andrew”;
-$name­>lastName = “Olivas”;
-print “My name is {$name­>getName()}.”;
+	$name = new Name();
+	$name­>firstName = “Andrew”;
+	$name­>lastName = “Olivas”;
+	print “My name is {$name­>getName()}.”;
 
 ____________________________________________________________________________________________________________________________________________________________
+
+	class Dog {
+		public $firstName;
+		public $lastName;
+		public $breed;
+
+	function __construct($title, $firstName, $lastName, $breed) {
+		$this­>firstName = $firstName;
+		$this­>lastName = $lastName;
+		$this­>breed = $breed;
+	}
+
+	function getName() {
+		return “{$this­>firstName}” .
+		“{$this­>lastName}”;
+		}
+	}
+
+	$dog1 = new Dog(“BooBoo”, “Iwao”, “EnglishBulldog”);
+	print “Dog 1: {$dog1­>getName()}\n;
