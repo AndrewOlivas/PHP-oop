79c85b58bec0ffc95c7d4b4f95518a7e5221bf42
Andrew Olivas <andrewrolivas@gmail.com>
2015-01-08 08:48:36 -0800

    changed properties

--- lesson1.php
+++ lesson1.php
@@ -19,36 +19,37 @@
 
 //The dog’s name should be BARRY ALLEN
 // example 2
+
 	class Dog {
-		public $firstName = “default name”;
-		public $lastName = “default last name”;
-		public $gender = “male”;
-		public $price = 0;
+		public $firstName2 = “default name”;
+		public $lastName2 = “default last name”;
+		public $gender2 = “male”;
+		public $price2 = 0;
 
 	function getName() {
-		return “{$this­>firstName}” .
-		“{$this­>lastName}”;
+		return “{$this­>firstName2}” .
+		“{$this­>lastName2}”;
 		}
 	}
 	$name = new Name();
-	$name­>firstName = “Andrew”;
-	$name­>lastName = “Olivas”;
+	$name­>firstName2 = “Andrew”;
+	$name­>lastName2 = “Olivas”;
 	print “My name is {$name­>getName()}.”;
 // example 3
 	class Chicken {
-		public $firstName = “default name”;
-		public $lastName = “default last name”;
-		public $gender = “male”;
-		public $price = 0;
+		public $firstName3 = “default name”;
+		public $lastName3 = “default last name”;
+		public $gender3 = “male”;
+		public $price3 = 0;
 
 	function getName() {
-		return “{$this­>firstName}” .
-		“{$this­>lastName}”;
+		return “{$this­>firstName3}” .
+		“{$this­>lastName3}”;
 		}
 	}
 	$chicken = new Name();
-	$chicken­>firstName = “nuggets”;
-	$chicken­>lastName = “boyd”;
+	$chicken­>firstName3 = “nuggets”;
+	$chicken­>lastName3 = “boyd”;
 	print “My Chickens name is {$name­>getName()}.”;
 
 ?>
@@ -75,44 +76,73 @@ ________________________________________________________________________________
 
 	$dog1 = new Dog(“Bobo”, “Swagins”, “SiberianHusky”);
 	print “Dog 1: {$dog1­>getName()}\n;
-// new example
-	class Armadillo {
-		public $firstName;
-		public $lastName;
-		public $breed;
 
-	function __construct($title, $firstName, $lastName, $breed) {
-		$this­>firstName = $firstName;
-		$this­>lastName = $lastName;
-		$this­>breed = $breed;
+// example 2
+
+	class Armadillo {
+		public $firstName2;
+		public $lastName2;
+		public $breed2;
+
+	function __construct($title, $firstName2, $lastName2, $breed2) {
+		$this­>firstName2 = $firstName2;
+		$this­>lastName2 = $lastName2;
+		$this­>breed2 = $breed2;
 	}
 
 	function getName() {
-		return “{$this­>firstName}” .
-		“{$this­>lastName}”;
+		return “{$this­>firstName2}” .
+		“{$this­>lastName2}”;
 		}
 	}
 
 	$armadillo = new Armadillo(“Mike”, “Rolo”, “GiantArmadillo”);
 	print “Armadillo”: {$armadillo­>getName()}\n;
 
-// new example
-	class Cat {
-		public $firstName;
-		public $lastName;
-		public $breed;
+// example 3
 
-	function __construct($title, $firstName, $lastName, $breed) {
-		$this­>firstName = $firstName;
-		$this­>lastName = $lastName;
-		$this­>breed = $breed;
+	class Cat {
+		public $firstName3;
+		public $lastName3;
+		public $breed3;
+
+	function __construct($title, $firstName3, $lastName3, $breed3) {
+		$this­>firstName3 = $firstName3;
+		$this­>lastName3 = $lastName3;
+		$this­>breed3 = $breed3;
 	}
 
 	function getName() {
-		return “{$this­>firstName}” .
-		“{$this­>lastName}”;
+		return “{$this­>firstName3}” .
+		“{$this­>lastName3}”;
 		}
 	}
 
 	$cat1 = new Dog(“Jeff”, “Chang”, “Tabby”);
 	print “Cat 1: {$cat1­>getName()}\n;
+	?>
+
+_____________________________________________________________________________________________________________________________________________
+
+// lesson 3
+
+	<?php
+	if (is_string())echo “It is a string”;
+	else
+	echo ‘It is not a string’;
+	var_dump(is_string(‘XYZ’);
+	var_dump(is_string(‘8008’);
+	var_dump(is_string(123);
+	var_dump(is_string(false);
+
+// example 2
+
+	if (is_string())echo “It is a string”;
+	else
+	echo ‘It is not a string’;
+	var_dump(is_string(‘XYZ’);
+	var_dump(is_string(‘8008’);
+	var_dump(is_string(123);
+	var_dump(is_string(false);
+
+?>
